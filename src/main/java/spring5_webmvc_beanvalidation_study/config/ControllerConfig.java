56a34ffcbffcfcba4810698eb664dd609496d676
nyLee <skdus2421@naver.com>
2021-05-21 15:06:14 +0900

    초기화

---
+++ src/main/java/spring5_webmvc_beanvalidation_study/config/ControllerConfig.java
@@ -0,0 +1,9 @@
+package spring5_webmvc_beanvalidation_study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"spring5_webmvc_beanvalidation_study.controller"})
+public class ControllerConfig {
+}
